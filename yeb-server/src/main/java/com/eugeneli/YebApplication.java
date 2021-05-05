30773a2becc0acbb02e4dcdded69ae024042e9b9
eugeneli <861269630@qq.com>
2021-05-05 11:02:07 +0800

    测试

--- yeb-server/src/main/java/com/eugeneli/YebApplication.java
+++ yeb-server/src/main/java/com/eugeneli/YebApplication.java
@@ -1,4 +1,4 @@
-package com.eugeneli.generator.server;
+package com.eugeneli;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
---
+++ yeb-server/src/main/java/com/eugeneli/service/impl/UserService.java
@@ -0,0 +1,11 @@
+package com.eugeneli.service.impl;
+
+/**
+ * @ClassName: UserService
+ * @Description:
+ * @Author EugeneLi
+ * @Date: 2021/5/5
+ * @Time: 10:59
+ */
+public class UserService {
+}
